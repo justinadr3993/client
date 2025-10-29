6add8037a806ca72495176548a0d44c054630d26
Justin Uminga <justinuminga@gmail.com>
2025-10-29 11:25:21 +0800

    cleanup

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -83,7 +83,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.mru1fhjk4cg"
+    "revision": "0.h5cc6l7i2v"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
